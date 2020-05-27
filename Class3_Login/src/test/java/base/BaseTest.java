0c1af698b4b07ac227ab050ce1f6cf16d1fbd03c
beloquintana <belo.quintana@gmail.com>
2020-05-27 15:54:30 -0300

    Upload Class3_Login project

---
+++ Class3_Login/src/test/java/base/BaseTest.java
@@ -0,0 +1,23 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver webDriver;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","resource/chromedriver.exe");
+        webDriver = new ChromeDriver();
+        webDriver.get("http://testfaceclub.com/ejercicios/");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        if(webDriver != null)
+            webDriver.quit();
+    }
+}
